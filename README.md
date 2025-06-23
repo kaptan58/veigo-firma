affa3f66c516993cc59f4eb995e16adf146937ca
kaptan58 <albaylogistik@hotmail.com>
2025-06-23 06:24:01 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# veigo-firma
+# Veigo Firma Paneli
+
+`npm install` ve ardından `npm run dev` komutlarıyla çalıştırabilirsiniz.
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Firma Paneli - VEIGO</h1>
+      <p>Giriş yapın, araçlarınızı yönetin, şoförleri takip edin.</p>
+    </div>
+  );
+}
